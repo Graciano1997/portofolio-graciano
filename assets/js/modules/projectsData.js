d267d4d9d0a5f823940cda35995034606065e191
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-09-17 09:19:23 +0100

    Add countries image

--- assets/js/modules/projectsData.js
+++ assets/js/modules/projectsData.js
@@ -95,7 +95,7 @@ const projects = [
   {
     id: 6,
     images: {
-      image: { path: '../assets/img/book.png', alt: 'Medical Illustration sets' },
+      image: { path: '../assets/img/covid.png', alt: 'pandemic countries situation' },
       btnClose: { path: '../assets/img/btnmedicalClose.svg', alt: 'close icon', id: 'modalClose' },
     },
     title: 'Today Covid-19 Pandemic Situation',
@@ -106,7 +106,7 @@ const projects = [
     **Everything in Real time, Fetching the data from a provided API**.`,
     button: {
       label: ['See live', 'See source'],
-      link: ['https://todays-pandemic-status-app.onrender.com/', 'https://github.com/Graciano1997/pandemic_react_capstone.git'],
+      link: ['https://pandemic-react-capstone.vercel.app/', 'https://github.com/Graciano1997/pandemic_react_capstone.git'],
       iconsPath: ['../assets/img/seeLive.svg', '../assets/img/seeGit.svg'],
     },
   }
