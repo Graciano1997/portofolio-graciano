21c7be4627a4caaf32dc626139be3e35c5fa1c6c
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-01-24 08:05:11 +0100

    add required changes

--- src/components/Network.jsx
+++ src/components/Network.jsx
@@ -8,22 +8,22 @@ import style from '../style/Headline.module.css';
 const Networks = () => (
   <ul className={style.socialMedia}>
     <li>
-      <a href=" https://github.com/Graciano1997" target="_blank" rel="noopener">
+      <a href=" https://github.com/Graciano1997" target="_blank" rel="noreferrer">
         <img src={github} alt="github icon" />
       </a>
     </li>
     <li>
-      <a href="https://www.linkedin.com/in/graciano-manuel-henrique-175527221/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/gracianohenrique/" target="_blank" rel="noreferrer">
         <img
           src={linkedin}
           alt="linkedin icon"
         />
       </a>
     </li>
-    <li><a href="https://wellfound.com/u/graciano-manuel-henrique" rel="noopener"><img src={angellist} alt="Angel List" /></a></li>
-    <li><a href="https://twitter.com/GracianoSoft" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter icon" /></a></li>
+    <li><a href="https://wellfound.com/u/graciano-manuel-henrique" target="_blank" rel="noreferrer"><img src={angellist} alt="Angel List" /></a></li>
+    <li><a href="https://twitter.com/GracianoSoft" target="_blank" rel="noreferrer"><img src={twitter} alt="twitter icon" /></a></li>
     <li>
-      <a href="https://medium.com/@gracianomanuelhenrique" target="_blank" rel="noopener noreferrer"><img src={medium} alt="medium icon" /></a>
+      <a href="https://medium.com/@gracianomanuelhenrique" target="_blank" rel="noreferrer"><img src={medium} alt="medium icon" /></a>
     </li>
   </ul>
 );
