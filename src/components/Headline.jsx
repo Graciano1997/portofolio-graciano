d808549878e1a00dc31af490fe167d5b9c102862
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-08-07 21:27:20 +0100

    add the new portfolio link

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -9,6 +9,11 @@ const Headline = () => (
       <Header />
       <Description />
       <Networks />
+      <div className={style.info}>
+        <p>Thank you for being here!</p>
+        <p>I would like to share that I have a new available portfolio website</p>
+        <a href="https://portfolio-zeta-ten-c0263zs4e4.vercel.app/">Please click here to visit for a new experience</a>
+      </div>
     </div>
   </section>
 );
